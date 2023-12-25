c49f02a40ef01b645b5749fcdabec4a6a15f77d7
dev-jmichael <flores.johnmichael.c.0511@gmail.com>
2023-12-26 01:01:39 +0800

    Change spacing

--- src/controllers/questionBankController.ts
+++ src/controllers/questionBankController.ts
@@ -8,6 +8,7 @@ export const createQuestionBank = async (request: FastifyRequest<{ Body: CreateQ
     try {
         const questionBankRequest: CreateQuestionBankRequest = request.body;
         const createdQuestionBank: Omit<QuestionBank, 'question_bank_id'> = await questionBankService.createQuestionBank(questionBankRequest);
+
         reply.status(201).send(successResponse(201, createdQuestionBank, 'Question bank created.'));
     } catch (error: any) {
         console.log(error)
@@ -19,6 +20,7 @@ export const getQuestionBank = async (request: FastifyRequest<{ Params: { questi
     try {
         const { questionBankId } = request.params;
         const questionBank: QuestionBank | undefined = await questionBankService.getQuestionBank(questionBankId);
+        
         reply.status(200).send(successResponse(200, questionBank));
     } catch (error: any) {
         console.log(error)
--- src/services/questionBankService.ts
+++ src/services/questionBankService.ts
@@ -7,7 +7,6 @@ import { CreateQuestionBankRequest } from "../dto/createQuestionBankRequest";
 const questionBankRepository = new QuestionBankRepository(db);
 
 const createQuestionBank = async (questionBankRequest: CreateQuestionBankRequest): Promise<Omit<QuestionBank, 'question_bank_id'>> => {
- 
     const questionBank: Omit<QuestionBank, 'question_bank_id'> = { ...questionBankRequest }
 
     const createdQuestionBank: Omit<QuestionBank, 'question_bank_id'> = await questionBankRepository.save(questionBank)
@@ -16,7 +15,6 @@ const createQuestionBank = async (questionBankRequest: CreateQuestionBankRequest
 }
 
 const getQuestionBank = async (questionBankId: string): Promise<QuestionBank | undefined> => {
-
     const foundQuestionBank: QuestionBank | undefined = await questionBankRepository.findById(questionBankId);
     
     if (!foundQuestionBank) {
